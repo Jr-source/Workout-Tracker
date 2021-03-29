d08a253a01f7199235d5672521186023a7bbf3df
Jacob Riley <jacobriley1987@gmail.com>
2021-03-28 23:11:05 -0700

    troubleshooting

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const PORT = 3000;
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(
+	process.env.MONGODB_URI || "mongodb://localhost/workout-tracker",
+ 	{
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false
+	}
+);
+
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
+
+app.listen(process.env.PORT, () => {
+	console.log(`App running on port ${PORT}!`);
+});
